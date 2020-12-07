8942ea66f840857a25b476d32c1228436e84af36
İlknur Ültanır <ilknur@gamerarena.com>
2020-12-07 07:12:07 +0300

    Update matrix

--- index.ts
+++ index.ts
@@ -1,38 +1,20 @@
-const _ = require('lodash');
-const { Matrix, NearestNeighbour } = require('./scripts');
+const fs = require('fs');
+const path = require('path');
+const { NearestNeighbour } = require('./scripts');
 
-const matrix = new Matrix([{
-  unit: 1,
-  corr: 1
-}, {
-  unit: 1,
-  corr: 2
-}, {
-  unit: 1,
-  corr: 3
-}, {
-  unit: 1,
-  corr: 4
-}, {
-  unit: 1,
-  corr: 5
-}, {
-  unit: 1,
-  corr: 6
-}]).calculateMatrix;
 
-console.log(matrix);
+let rawData = fs.readFileSync(path.join(__dirname, 'matrix.json'));
+let matrix = JSON.parse(rawData);
 
-// Nearest Neighbour Test
-const paths = new NearestNeighbour([
-  [0, 132, 217, 164, 58],
-  [132, 0, 290, 201, 79],
-  [217, 290, 0, 113, 303],
-  [164, 201, 113, 0, 196],
-  [58, 79, 303, 196, 0]
-]).tour();
+const paths = new NearestNeighbour(matrix).tour()
 
-// const paths = new NearestNeighbour(matrix);
 
-// console.log(paths);
+// const paths = new NearestNeighbour([
+//   [0, 132, 217, 164, 58],
+//   [132, 0, 290, 201, 79],
+//   [217, 290, 0, 113, 303],
+//   [164, 201, 113, 0, 196],
+//   [58, 79, 303, 196, 0]
+// ]).tour();
 
+console.log(paths);
--- scripts/manhattan.ts
+++ scripts/manhattan.ts
@@ -1,3 +1,5 @@
+const { isEqual } = require('lodash');
+
 type Point = {
   corr: number,
   unit: number,
@@ -103,30 +105,47 @@ export class Manhattan {
     }
   }
 
-  distance(givenFrom = this.from, givenTo = this.to, givenFromDir = this.fromDir, givenToDir = this.toDir) {
-    const { fromDir, toDir, from, to } = this;
+  distance(givenFrom = this.from,
+    givenTo = this.to,
+    givenFromDir = this.fromDir,
+    givenToDir = this.toDir): number | undefined | Error {
+
     if (givenFrom !== undefined && givenTo !== undefined) {
       let distance;
 
-      if (from.corr === to.corr) {
-        if (Math.round(from.unit / 2) === Math.round(to.unit / 2)) {
-          distance = 0;
-        } else {
-          if (givenFromDir && givenTo.unit > givenFrom.unit || !givenFromDir && givenTo.unit < givenTo.unit) {
-            distance = this.handleSameCorridor(true);
-          } else if (givenFromDir && givenFrom.unit > givenTo.unit || !givenFromDir && givenFrom.unit < givenTo.unit) {
-            distance = this.handleSameCorridor();
-          } else {
+      // starts from initial and goes to initial
+      if (givenFrom.unit === 0 && givenFrom.corr === 0 && givenTo.unit === 0 && givenTo.corr === 0) {
+        distance = 0;
+
+        // starts from initial
+      } else if (givenFrom.unit === 0 && givenFrom.corr === 0) {
+        distance = initial({ unit: 0, corr: 0 }, givenTo);
+
+        // goes from initial
+      } else if (givenTo.unit === 0 && givenTo.corr === 0) {
+        distance = initial(givenFrom, { unit: 0, corr: 0 });
+
+        // travels from unit o unit
+      } else {
+        if (givenFrom.corr === givenTo.corr) {
+          if (Math.round(givenFrom.unit / 2) === Math.round(givenFrom.unit / 2)) {
             distance = 0;
+          } else {
+            if (givenFromDir && givenTo.unit > givenFrom.unit || !givenFromDir && givenTo.unit < givenTo.unit) {
+              distance = this.handleSameCorridor(true);
+            } else if (givenFromDir && givenFrom.unit > givenTo.unit || !givenFromDir && givenFrom.unit < givenTo.unit) {
+              distance = this.handleSameCorridor();
+            } else {
+              distance = 0;
+            }
           }
+        } else if (givenFrom.corr !== givenTo.corr && givenFromDir !== givenToDir) {
+          distance = this.handleDiffCorridorsDiffDirs();
+        } else if (givenFrom.corr !== givenTo.corr && givenFromDir === givenToDir) {
+          distance = this.handleDiffCorridorsSameDir();
         }
-      } else if (givenFrom.corr !== givenTo.corr && givenFromDir !== givenToDir) {
-        distance = this.handleDiffCorridorsDiffDirs();
-      } else if (givenFrom.corr !== givenTo.corr && givenFromDir === givenToDir) {
-        distance = this.handleDiffCorridorsSameDir();
       }
 
-      // console.log(`From ${givenFrom.corr}-${givenFrom.unit} to ${givenTo.corr}-${givenTo.unit} - ${distance}`);
       return distance;
     }
 
@@ -134,78 +153,113 @@ export class Manhattan {
   }
 }
 
-export class Initial {
-  point: Point;
-  center: Point;
-  from?: boolean;
+function initial(from: Point, to: Point) {
+  // Depot variables
+  const corridors = 6;
+  const units = 28;
 
-  constructor(point: Point, from = true) {
-    this.point = point;
-    this.center = { unit: 1, corr: 4 };
-    this.from = from;
+  // Lateral variables
+  const corner = 2;
+  const lateral = 1;
+
+  function corrIsUpwards(corr: number) {
+    return corr % 2 === 1;
   }
 
-  findDirection() {
-    return this.point.corr % 2 === 1;
-  }
-
-  findNearestDownCorr() {
-    if (this.point.corr >= this.center.corr) {
-      return this.point.corr - 1;
+  function distance(from: number[], to: number[]) {
+    // doesn't move
+    if (isEqual(from, to)) {
+      return 0;
     }
-    return this.point.corr + 1;
-  }
 
-  findLateralLength(corr: number) {
-    const passedCors = Math.abs(this.center.corr - corr);
-    const lateralLength = 2;
-    const cornerLength = 2;
+    // starts from initial point
+    else if (isEqual(from, [0, 0])) {
+      const midPoint = (corner * corridors + (corridors - 1) * lateral * 2) / 2;
+      const corrLateralDistance =
+        (to[0] - 1) * corner + (to[0] - 1) * lateral * 2;
 
-    if (corr >= this.center.corr) {
-      return ((passedCors + 1) * cornerLength) + (passedCors * lateralLength) + 0.5 * lateralLength;
-    } else {
-      return ((passedCors + 1) * cornerLength) + (passedCors * lateralLength) - cornerLength - 0.5 * lateralLength;
-    }
-  }
+      let lateralLength = 0;
 
-  get calculateLength() {
-    const dir = this.findDirection();
-
-    let distance;
-    if (this.from) {
-      if (dir) {
-        const nearestCorr = this.findNearestDownCorr();
-        const nearestCorrExitLength = new Manhattan({
-          unit: this.point.unit, corr: this.point.corr
-        }, {
-          unit: 1, corr: nearestCorr
-        }).distance();
-
-        if (typeof nearestCorrExitLength === 'number') {
-          distance = nearestCorrExitLength + this.findLateralLength(nearestCorr);
+      if (corrIsUpwards(to[0])) {
+        if (midPoint > corrLateralDistance) {
+          lateralLength = midPoint - corrLateralDistance;
+        } else {
+          lateralLength = corrLateralDistance - midPoint + corner;
         }
       } else {
-        distance = this.findLateralLength(this.point.corr) - 1 + Math.round(this.point.unit / 2);
-      }
-    } else {
-      if (dir) {
-        distance = this.findLateralLength(this.point.corr) + Math.round(this.point.unit / 2);
-      } else {
-        const nearestCorr = this.findNearestDownCorr();
-        const nearestCorrExitLength = new Manhattan({
-          unit: 1, corr: nearestCorr
-        }, {
-          unit: this.point.unit, corr: this.point.corr
-        }).distance();
-
-        if (typeof nearestCorrExitLength === 'number') {
-          distance = nearestCorrExitLength + this.findLateralLength(nearestCorr) + 1;
+        if (to[0] === corridors / 2 + 1) {
+          lateralLength = 2 * corner + 2 * lateral + lateral + corner;
+        } else if (midPoint > corrLateralDistance) {
+          lateralLength = 2 * corner + 2 * lateral + lateral + corner;
+        } else {
+          lateralLength = 4 * corner + 4 * lateral + lateral;
         }
       }
+
+      // has to go upwards
+      if (corrIsUpwards(to[0])) {
+        return lateralLength + Math.round(to[1] / 2);
+      } else {
+        return lateralLength + units + Math.round(to[1] / 2) - 1;
+      }
     }
-    console.log({ distance, point: this.point });
-    return distance;
+
+    // goes back to initial point
+    else if (isEqual([0, 0], to)) {
+      const midPoint = (corner * corridors + (corridors - 1) * lateral * 2) / 2;
+      const corrLateralDistance =
+        from[0] * corner + (from[0] - 1) * lateral * 2;
+
+      let lateralLength = 0;
+
+      // calculate lateral length
+      // goes upwards
+      if (corrIsUpwards(from[0])) {
+        // exception
+        if (from[0] === corridors / 2) {
+          lateralLength = 3 * corner + 3 * lateral;
+        }
+
+        // if corr is after midPoint
+        else if (corrLateralDistance > midPoint) {
+          lateralLength = corrLateralDistance - midPoint + corner;
+        }
+
+        // if corr is before midPoint
+        else {
+          lateralLength = midPoint - corrLateralDistance + 2 * corner;
+        }
+      }
+      // goes downwards
+      else {
+        // if corr is after midPoint
+        if (corrLateralDistance > midPoint) {
+          lateralLength = corrLateralDistance - midPoint - corner;
+        }
+
+        // if corr is before midPoint
+        else {
+          lateralLength = midPoint - corrLateralDistance;
+        }
+      }
+
+      // calculate distance
+      // has to go upwards
+      if (corrIsUpwards(from[0])) {
+        const passedCorridor = units / 2;
+        const exitCorr = units / 2 - Math.round(from[1] / 2);
+
+        return exitCorr + passedCorridor + lateralLength;
+      }
+
+      // has to go downwards
+      else {
+        return lateralLength + corner + Math.round(from[1] / 2) - 1;
+      }
+    }
   }
+
+  return distance([from.corr, from.unit], [to.corr, to.unit]);
 }
 
 
